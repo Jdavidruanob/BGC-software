9cd33041d3c521e30aa22433c604ee4ee8fb2b1d
Jdavidrb <jdavidruanob@gmail.com>
2025-06-22 18:34:19 -0500

    Iconos navbar correctamente funcionando

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+PRIMARY_COLOR = "#153A66"     # Azul oscuro (fondo navbar)
+SECONDARY_COLOR = "#8C5B2F"   # Marrón (hover y botón activo)
+TEXT_COLOR = "#FFFFFF"        # Texto blanco
